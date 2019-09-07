7d71e3692075bfaec3851f23900eb130c08a8625
naoki1510 <naoshio.2002@gmail.com>
2019-09-07 20:09:25 +0900

    build.phpの内容作成

--- build.php
+++ build.php
@@ -1,2 +1,10 @@
 <?php
 ## TODO: write build script
+
+var_dump($themes = glob('./src/themes/*.scss'));
+
+foreach ($themes as $theme) {
+    system("npx node-sass --include-path scss $theme ./bizarre-foods-" . mb_substr(basename($theme), 0, strlen(basename($theme)) - 5) . "/style.css");
+    //var_dump($output);
+    //echo implode("\r\n", $output) . "\r\n";
+}
